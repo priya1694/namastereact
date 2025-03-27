106249ed082566bc488b543f14bc7f6be6fba481
priya1694 <priyasn1994@gmail.com>
2025-03-27 16:31:13 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,14 @@
+// how to create multiple div in react like this 
+{/* <div id="parent">
+    <div id="child">
+        <h1>i'm an h1 tag</h1>
+        </div>
+    </div> */}
+
+const parent = React.createElement("div",{id:"parent"},
+React.createElement("div",{id:"child"},
+ [React.createElement("h1",{},"i'm an h1 tag"),React.createElement("h2",{},"i'm an h2 tag")],));
+
+const heading = React.createElement("h1",{id:"heading"},"Hello world from react!")
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent);
